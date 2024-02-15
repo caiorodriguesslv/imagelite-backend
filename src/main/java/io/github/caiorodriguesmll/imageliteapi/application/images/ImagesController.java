db34b16bb8fc7fe8a8565676dbd40a873b3830cf
caio rodrigues <caiorodriguesmll@gmail.com>
2024-02-15 20:35:00 -0300

    Image Endpoint Created

--- src/main/java/io/github/caiorodriguesmll/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/caiorodriguesmll/imageliteapi/application/images/ImagesController.java
@@ -1,4 +1,26 @@
 package io.github.caiorodriguesmll.imageliteapi.application.images;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/images")
 public class ImagesController {
+
+    @PostMapping
+    public ResponseEntity save(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("tags") List<String> tags
+            ){
+        return ResponseEntity.ok().build();
+    }
 }
